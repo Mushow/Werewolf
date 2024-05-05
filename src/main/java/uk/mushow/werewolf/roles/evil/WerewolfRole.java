86d7e0a062e4147e7e98f923a54478e81c56c58f
Antoine James <antoine@mushow.uk>
2024-05-05 21:22:19 +0200

    Add WerewolfRole texture

--- src/main/java/uk/mushow/werewolf/roles/evil/WerewolfRole.java
+++ src/main/java/uk/mushow/werewolf/roles/evil/WerewolfRole.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class WerewolfRole implements Role {
 
-    private final String texture = "";
+    private final String texture = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzY4ZDQzMTI5MzliYjMxMTFmYWUyOGQ2NWQ5YTMxZTc3N2Y4ZjJjOWZjNDI3NTAxY2RhOGZmZTNiMzY3NjU4In19fQ==";
 
     @Override
     public String getRoleName() {
