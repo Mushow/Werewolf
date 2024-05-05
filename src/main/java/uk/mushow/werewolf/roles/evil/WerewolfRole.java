872859e087dc491a2d9434169986f5a764623269
Antoine James <antoine@mushow.uk>
2024-05-05 20:24:59 +0200

    Initialize the WerewolfRole

---
+++ src/main/java/uk/mushow/werewolf/roles/evil/WerewolfRole.java
@@ -0,0 +1,37 @@
+package uk.mushow.werewolf.roles.evil;
+
+import org.bukkit.ChatColor;
+import uk.mushow.werewolf.roles.Role;
+
+import java.util.List;
+
+public class WerewolfRole implements Role {
+
+    @Override
+    public String getRoleName() {
+        return "Werewolf";
+    }
+
+    @Override
+    public List<String> getRoleDescription() {
+        return List.of("As a Werewolf, you are disguised as a villager during the day but transform at night to hunt the villagers.",
+                "Your goal is to secretly eliminate all the villagers without being discovered.",
+                "Work closely with other Werewolves to strategize and survive until you outnumber the villagers.");
+    }
+
+    @Override
+    public ChatColor getRoleColor() {
+        return ChatColor.DARK_RED;
+    }
+
+    @Override
+    public boolean isGood() {
+        return false;
+    }
+
+    @Override
+    public void performNightAction() {
+
+    }
+
+}
