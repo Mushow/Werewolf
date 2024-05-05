42905c9196d4716cc351d79d34c73156ae0a6618
Antoine James <antoine@mushow.uk>
2024-05-05 21:44:47 +0200

    Rearrange methods order

--- src/main/java/uk/mushow/werewolf/roles/good/VillagerRole.java
+++ src/main/java/uk/mushow/werewolf/roles/good/VillagerRole.java
@@ -15,16 +15,17 @@ public class VillagerRole implements Role {
     }
 
     @Override
+    public ChatColor getRoleColor() {
+        return ChatColor.GRAY;
+    }
+
+    @Override
     public List<String> getRoleDescription() {
         return List.of("As a Villager, you are a simple and innocent villager who must work with the other villagers to identify and eliminate the Werewolves.",
                 "Your goal is to eliminate all the Werewolves before they outnumber the villagers.",
                 "Use your keen observation skills to identify suspicious behavior and vote to eliminate the Werewolves during the day.");
     }
 
-    @Override
-    public ChatColor getRoleColor() {
-        return ChatColor.GRAY;
-    }
 
     @Override
     public boolean isGood() {
