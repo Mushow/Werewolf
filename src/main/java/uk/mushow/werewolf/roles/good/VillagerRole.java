af50a073e7a9ee345d34ff4a92ccdaad693b760c
Antoine James <antoine@mushow.uk>
2024-05-05 20:24:06 +0200

    Initialize the VillagerRole

---
+++ src/main/java/uk/mushow/werewolf/roles/good/VillagerRole.java
@@ -0,0 +1,37 @@
+package uk.mushow.werewolf.roles.good;
+
+import org.bukkit.ChatColor;
+import uk.mushow.werewolf.roles.Role;
+
+import java.util.List;
+
+public class VillagerRole implements Role {
+
+    @Override
+    public String getRoleName() {
+        return "Villager";
+    }
+
+    @Override
+    public List<String> getRoleDescription() {
+        return List.of("As a Villager, you are a simple and innocent villager who must work with the other villagers to identify and eliminate the Werewolves.",
+                "Your goal is to eliminate all the Werewolves before they outnumber the villagers.",
+                "Use your keen observation skills to identify suspicious behavior and vote to eliminate the Werewolves during the day.");
+    }
+
+    @Override
+    public ChatColor getRoleColor() {
+        return ChatColor.GRAY;
+    }
+
+    @Override
+    public boolean isGood() {
+        return true;
+    }
+
+    @Override
+    public void performNightAction() {
+
+    }
+
+}
